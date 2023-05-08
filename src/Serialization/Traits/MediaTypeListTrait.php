076fdf52d9f96b599370b3f799897959e3954b45
Renaud Guillard <dev@nore.fr>
2023-05-08 21:08:42 +0200

    fix PHPDoc @package tags

--- src/Serialization/Traits/MediaTypeListTrait.php
+++ src/Serialization/Traits/MediaTypeListTrait.php
@@ -4,7 +4,7 @@
  * Copyright © 2021 by Renaud Guillard (dev@nore.fr)
  * Distributed under the terms of the MIT License, see LICENSE
  *
- * #package Data
+ * @package Data
  */
 namespace NoreSources\Data\Serialization\Traits;
 
--- tests/Serialization/SerializerTest.php
+++ tests/Serialization/SerializerTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2021 by Renaud Guillard (dev@nore.fr)
  * Distributed under the terms of the MIT License, see LICENSE
  *
- * #package Data
+ * @package Data
  */
 namespace NoreSources\Data\Test;
 
