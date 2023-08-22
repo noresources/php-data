20d527e05fa731d5b01947f5ce9f0a9e47c4ee17
Renaud Guillard <dev@nore.fr>
2023-08-22 21:59:28 +0200

    Data primitification fallback method
    Add a default behavior to primitifyData() when media type
    is not provided or does not indicate any relevant informations.

--- src/Serialization/Traits/PrimitifyTrait.php
+++ src/Serialization/Traits/PrimitifyTrait.php
@@ -8,23 +8,34 @@
  */
 namespace NoreSources\Data\Serialization\Traits;
 
+use NoreSources\Container\Container;
 use NoreSources\Data\Primitifier;
 use NoreSources\MediaType\MediaTypeInterface;
 use NoreSources\Type\TypeConversion;
 
+/**
+ * Reference method to transform any kind of value to a POD value
+ */
 trait PrimitifyTrait
 {
 
+	/**
+	 *
+	 * @param mixed $data
+	 *        	Data to convert to POD value
+	 * @param MediaTypeInterface $mediaType
+	 * @return mixed|unknown|unknown[]|unknown
+	 */
 	public function primitifyData($data,
 		MediaTypeInterface $mediaType = null)
 	{
 		if (!$mediaType)
-			return $data;
+			return $this->primitifyDataFallback($data);
 
 		$key = 'preprocess-depth';
 		$p = $mediaType->getParameters();
 		if (!$p->has($key))
-			return $data;
+			return $this->primitifyDataFallback($data);
 
 		$maxDepth = TypeConversion::toInteger($p->get($key));
 		$primitifier = $this->createPrimitifier();
@@ -32,6 +43,24 @@ trait PrimitifyTrait
 			return $data;
 		$primitifier->setRecursionLimit($maxDepth);
 		return $primitifier($data);
+	}
+
+	/**
+	 * Default data primitification method when media type is not provided or does not gave relevant
+	 * informations.
+	 *
+	 * @param unknown $data
+	 *        	Data to convert to POD value
+	 * @return mixed
+	 */
+	public function primitifyDataFallback($data)
+	{
+		if (!Container::isTraversable($data))
+			return $data;
+		$map = [];
+		foreach ($data as $key => $value)
+			$map[$key] = $value;
+		return $map;
 	}
 
 	/**
